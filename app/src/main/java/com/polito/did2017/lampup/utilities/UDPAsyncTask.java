048af1c958fcecd5fcf19f8129f3098aa3ebc1fa
mugolini4 <matilde.ugolini@gmail.com>
2018-07-17 19:07:59 +0200

    - Aggiunto UDP send packet per accendere lamps con switch direttamente dalla lista -> funziona

--- app/src/main/java/com/polito/did2017/lampup/utilities/UDPAsyncTask.java
+++ app/src/main/java/com/polito/did2017/lampup/utilities/UDPAsyncTask.java
@@ -113,7 +113,7 @@ public class UDPAsyncTask extends AsyncTask<Object, String, Integer> {
 
         try {
             socket.send(p);//properly able to send data. i receive data to server
-            Log.e( "UDP SENDER TASK", "Inviato UDP a: " + lampIP + ", message: " + message );
+            Log.e( "UDP SENDER TASK", "Inviato UDP a: " + lampIP + ", message: " + messageStr );
 
         } catch (IOException e) {
             e.printStackTrace();
